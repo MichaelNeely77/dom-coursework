53f3afca0e7cd1c528574e1a5881427305897c3f
Michael Neely <michael@michaelneely.net>
2023-02-10 08:16:12 -0500

    Learned more about DOM manipulation

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const h1 = document.getElementById('main-title');
 h1.textContent = "Some new title!";
 h1.style.color = 'purple';
 
-const li = document.querySelector('li:last-of-type');
-console.dir(li);
+const section =document.querySelector('section');
+// section.style.backgroundColor = 'blue';
+section.style.backgroundColor = '';
+section.className = 'red-bg';
 
-const listItemElements = document.getElementsByTagName('li');
+const button = document.querySelector('button');
 
-for (const listItemEl of listItemElements) {
+button.addEventListener('click', () => {
+    if( section.className === 'red-bg visible') {
+        section.className = 'red-bg invisible';
+    } else {
+        section.className = 'red-bg visible';
+    }
 
-    // console.dir(listItemEl);
+});
 
-}
